d58e8baaa4f3214ec16886adf581cb0b04d5d36d
Solarcow <Alex.Kamrath@live.com>
2016-07-27 11:01:18 +1000

    add global variable for accessing CSV

--- data/seeds.rb
+++ data/seeds.rb
@@ -5,4 +5,12 @@ require 'faker'
 
 def db_seed
   # Your code goes here!
+  id = 1
+  10.times do 
+  	name = Faker::Commerce.product_name
+  	brand = Faker::Company.name
+  	price = Faker::Commerce.price
+  	Product.new(id: id, brand: brand, name: name, price: price)
+  	id += 1
+  end
 end
--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -4,4 +4,16 @@ require 'csv'
 
 class Udacidata
   # Your code goes here!
+  attr_reader :data_path
+  @@data_path = File.dirname(__FILE__) + "/../data/data.csv"
+
+  class << self
+  
+	  def create(args = {})
+	  	product = self.new(args)
+	  	CSV.open(@@data_path, "a+") do |csv|
+	  		csv << [product.id, product.brand, product.name, product.price]
+	  	end
+	  end
+	end
 end
