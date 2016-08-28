97fe64c5e4224f0800e9ec12b2ac4130eec63a58
Solarcow <Alex.Kamrath@live.com>
2016-08-28 12:01:29 +1000

    complete analyzable and verify app runs

--- lib/analyzable.rb
+++ lib/analyzable.rb
@@ -1,3 +1,42 @@
+
+
 module Analyzable
   # Your code goes here!
+  def average_price(products)
+  	price_array = []
+
+  	products.each do |product|
+  		price_array << product.price.to_f
+  	end
+  	sum = 0
+  	price_array.each do |price|
+  		sum += price
+  	end
+  	return ( sum / products.length).round(2)
+  end
+
+  def print_report(products)
+    report = {}
+    products.each do |product|
+      puts "Brand: #{product.brand}, Name: #{product.name}, Price #{product.price}"
+    end
+    return report.to_s
+  end
+
+  def count_by_brand(products)
+    brand = Hash.new(0)
+    products.each do |product|
+      brand[product.brand] += 1
+    end
+    return brand
+  end
+
+  def count_by_name(products)
+    name = Hash.new(0)
+    products.each do |product|
+      name[product.name] += 1
+    end
+    return name
+  end
+
 end
--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -9,13 +9,27 @@ class Udacidata
 
   create_finder_methods :brand, :name
 
+	  def update(opts = {})
+	  	@id = self.id
+	  	@brand = opts[:brand] || self.brand
+	  	@name = opts[:name] || self.name
+	  	@price = opts[:price] || self.price
+	  	self.class.destroy(@id)
+
+	  	CSV.open(@@data_path, "a+") do |csv|
+	  		csv << [@id, @brand, @name, @price]
+	  	end
+	  	return self
+
+	  end
+
   class << self
   
 	  def create(opts = {})
 	  	brand = opts[:brand]
 	  	name = opts[:name]
 	  	price = opts[:price]
-	  	product = self.new(brand: brand, name:name, price: price)
+	  	product = self.new(brand: brand, name: name, price: price)
 
 	  	CSV.open(@@data_path, "a+") do |csv|
 	  		csv << [product.id, product.brand, product.name, product.price]
@@ -23,10 +37,24 @@ class Udacidata
 	  	return product
 	  end
 
+	  def destroy(id)
+	  	product = self.find(id)
+
+	  	table = CSV.table(@@data_path)
+	  	table.delete_if do |row|
+	  		row[:id] == id
+	  	end
+
+	  	File.open(@@data_path, 'w') do |row|
+	  		row.write(table.to_csv)
+	  	end
+	  	product
+	  end
+
 	  def all
 	  	data = []
 	  	CSV.foreach(@@data_path, headers:true) do |row|
-	  		id, brand, name, price = row["id"], row["brand"], row["name"], row["price"]
+	  		id, brand, name, price = row["id"], row["brand"], row["product"], row["price"]
 	  		data << Product.new(id: id, brand: brand, name: name, price: price)
 	  	end
 	  	return data
@@ -54,24 +82,20 @@ class Udacidata
 	  			return search
 	  		end
 	  	end
+	  	raise ProductNotFoundError, "The ID: #{id} can't be found"
 	  end
 
-	  def destroy(id)
-	  	product = self.find(id)
-
-	  	table = CSV.table(@@data_path)
-	  	table.delete_if do |row|
-	  		row[:id] == id
+	  def where(args = {})
+	  	@products = []
+	  	self.all.each do |product|
+	  		brand = product.brand
+	  		name = product.name
+	  		if (brand == args[:brand] || name == args[:name])
+	  			@products << product
+	  		end
 	  	end
-
-	  	File.open(@@data_path, 'w') do |row|
-	  		row.write(table.to_csv)
-	  	end
-	  	product
+	  	return @products
 	  end
-
-
-
 
 	end
 end
